691b7defd113f7e7f97bf257f287eafb2abc4b71
srinethrayadav-design <srinethrayadav@gmail.com>
2026-06-03 16:33:32 +0530

    Create Pyramid_ Pattern.py

---
+++ Pyramid_ Pattern.py
@@ -0,0 +1,9 @@
+# Pyramid Pattern Program
+
+rows = int(input("Enter the number of rows: "))
+
+for i in range(1, rows + 1):
+    spaces = rows - i
+    stars = 2 * i - 1
+
+    print(" " * spaces + "*" * stars)
